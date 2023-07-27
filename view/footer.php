eaddd599a037f4be1d250345d4dc9ea8128b59b8
doancarlosembara <carlosdoanembara@gmail.com>
2023-07-27 16:26:45 +0700

    Final

---
+++ view/footer.php
@@ -0,0 +1,9 @@
+	<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Boarding School Student Tracker</b>
+        </div>
+        <strong><a href="itsourcecode.com">BOOSTER</a></strong>
+     </footer>
+      
+  </body>
+</html>
